ba6e2f10a99e49e1cf8d878741404839ccbf7d62
Qiusheng Wu <giswqs@gmail.com>
2022-04-05 12:06:05 -0400

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/geospatial-ml',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
